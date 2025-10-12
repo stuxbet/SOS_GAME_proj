01524e203f98a13e2e11667a82f506af5aac22ca
Luke <lukemalcom04@icloud.com>
2025-10-12 18:19:23 -0500

    feat: board mockup

--- src/sprint0gui.tsx
+++ src/sprint0gui.tsx
@@ -1,18 +1,46 @@
 import React from 'react';
-import {Box, Checkbox, FormControlLabel, Radio, RadioGroup, Typography} from '@mui/material';
+import {Box, Button, Checkbox, FormControlLabel, Radio, RadioGroup, Typography} from '@mui/material';
+import {SosGame} from './features/models';
 
+const game = new SosGame(3);
 
 export const Gui: React.FC = () => {
+  const handleMove = (row: number, col: number) => {
+    game.place(row, col, 'S');
+  };
+
   return (
-    <Box sx={{p: 4}}>
+    <Box sx={{p: 4, maxWidth: 720, width: '100%', mx: 'auto'}}>
       <Typography variant="h4" gutterBottom>Sprint 0 Simple GUI</Typography>
       <hr />
       <FormControlLabel control={<Checkbox />} label="Check me" />
 
-      <RadioGroup name="sprint0-radio-group">
-        <FormControlLabel value="option1" control={<Radio />} label="Option 1" />
-        <FormControlLabel value="option2" control={<Radio />} label="Option 2" />
+      <RadioGroup name="Game Mode:">
+        <FormControlLabel value="option1" control={<Radio />} label="Quickmatch" />
+        <FormControlLabel value="option2" control={<Radio />} label="General" />
       </RadioGroup>
+      <Box
+        display="grid"
+        gridTemplateColumns={`repeat(${game.size}, 3.5rem)`}
+        gridAutoRows="3.5rem"
+        gap={1.5}
+        justifyContent="center"
+        mt={3}
+      >
+        {game.board.flatMap((row, r) =>
+          row.map((cell, c) => (
+            <Button
+              key={`${r}-${c}`}
+              variant="outlined"
+              onClick={() => handleMove(r, c)}
+              sx={{minWidth: 0, width: '100%', height: '100%'}}
+            >
+              {cell ?? ''}
+            </Button>
+          )),
+        )}
+      </Box>
     </Box>
+    
   );
 };
